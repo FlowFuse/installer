70f547a6ff2bb684b90a50426eaccb9e99fdd9ed
Ben Hardill <ben@flowforge.com>
2022-01-23 12:42:58 +0000

    Update batch files to support being double clicked
    Added pauses at exit points so messages can be seen
    
    Also added engine to app/package.json

--- bin/flowforge.bat
+++ bin/flowforge.bat
@@ -1,14 +1,11 @@
 @echo off
 
-SETLOCAL
-
 set F_DRIVE=%~d0
 set F_PATH=%~dp0
 
 %F_DRIVE%
 cd %F_PATH%
 cd ..
-set FLOWFORGE_HOME=%CWD%
+SET FLOWFORGE_HOME=%CD%
 
 .\app\node_modules\.bin\flowforge.cmd
-
